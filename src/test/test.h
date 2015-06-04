bd5c083a717468f7ba90a5a67aa1e06365132c7b
lintong <lintong724@163.com>
2015-06-04 11:45:28 +0800

    添加了详细的测试数据

--- src/test/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_CJSON_TEST_HEADER
-#define MY_CJSON_TEST_HEADER
-#include <string>
-bool TestArray();
-bool TestObject();
-void TestByFile(const std::string &ifile, const std::string &ofile);
-#endif
